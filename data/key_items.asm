c203a13de0d5b3b4ca3102460263b8d7414eade1
TheFakeMateo <skeetendomateo@gmail.com>
2015-03-13 22:22:21 -0500

    Make fossils and Sun Stone not key items

--- data/key_items.asm
+++ data/key_items.asm
@@ -2,9 +2,9 @@ KeyItemBitfield: ; e799 (3:6799)
 	db %11110000
 	db %00000001
 	db %11110000
-	db %01001111
+	db %00001111 ; Make old amber not a key item (2nd from left)
 	db %00000000
-	db %10011111
+	db %10010100 ; Make Dome and Helix Fossil not key items (last 2 from right) and Sun Stone (4th from right)
 	db %00000000
 	db %11000000
 	db %11110000
