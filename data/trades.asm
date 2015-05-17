2b658d57cee6b574023b03ac15e2deed88a7911e
TheFakeMateo <skeetendomateo@gmail.com>
2015-05-17 16:22:57 -0500

    Label trade data

--- data/trades.asm
+++ data/trades.asm
@@ -1,14 +1,14 @@
 TradeMons: ; 71b7b (1c:5b7b)
 ; givemonster, getmonster, textstring, nickname (11 bytes), 14 bytes total
 
-	db NIDORINO,  NIDORINA,  0,"TERRY@@@@@@"
-	db ABRA,      MR_MIME,   0,"MARCEL@@@@@"
-	db PIKACHU,   TOGEPI,    1,"SHELLY@@@@@"
-	db PONYTA,    SEEL,      0,"SAILOR@@@@@"
-	db SPEAROW,   FARFETCH_D,2,"DUX@@@@@@@@"
-	db SLOWBRO,   LICKITUNG, 0,"MARC@@@@@@@"
-	db POLIWHIRL, JYNX,      1,"LOLA@@@@@@@"
-	db RAICHU,    ELECTRODE, 1,"DORIS@@@@@@"
-	db VENONAT,   TANGELA,   2,"CRINKLES@@@"
-	db NIDORAN_M, NIDORAN_F, 2,"SPOT@@@@@@@"
+	db NIDORINO,  NIDORINA,  0,"TERRY@@@@@@" ; can find in wild
+	db ABRA,      MR_MIME,   0,"MARCEL@@@@@" ; can find in wild
+	db PIKACHU,   TOGEPI,    1,"SHELLY@@@@@" ; Only place to find!
+	db PONYTA,    SEEL,      0,"SAILOR@@@@@" ; can find in wild
+	db SPEAROW,   FARFETCH_D,2,"DUX@@@@@@@@" ; can find in wild
+	db SLOWBRO,   LICKITUNG, 0,"MARC@@@@@@@" ; can find in wild
+	db POLIWHIRL, JYNX,      1,"LOLA@@@@@@@" ; can find in wild
+	db RAICHU,    ELECTRODE, 1,"DORIS@@@@@@" ; can find in wild
+	db VENONAT,   TANGELA,   2,"CRINKLES@@@" ; can find in wild
+	db NIDORAN_M, NIDORAN_F, 2,"SPOT@@@@@@@" ; can find in wild
 
