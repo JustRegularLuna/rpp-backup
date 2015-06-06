7cb02f60805a32a06fde671a96ad42becbca7ecf
TheFakeMateo <skeetendomateo@gmail.com>
2015-06-06 15:21:05 -0500

    List location of trades

--- data/trades.asm
+++ data/trades.asm
@@ -1,14 +1,14 @@
 TradeMons: ; 71b7b (1c:5b7b)
 ; givemonster, getmonster, textstring, nickname (11 bytes), 14 bytes total
 
-	db NIDORINO,  NIDORINA,  0,"TERRY@@@@@@" ; can find in wild
-	db ABRA,      SNEASEL,   0,"SHADOW@@@@@" ; Only place to find!
-	db PIKACHU,   TOGEPI,    1,"SHELLY@@@@@" ; Only place to find!
-	db PONYTA,    CHARMANDER,0,"FLARE@@@@@@" ; Only place (if not starter)
-	db POLIWAG,   SQUIRTLE,  2,"CRUSHER@@@@" ; Only place (if not starter)
-	db SLOWBRO,   LICKITUNG, 0,"MARC@@@@@@@" ; can find in wild
-	db ODDISH,    BULBASAUR, 1,"FIONA@@@@@@" ; Only place (if not starter)
-	db HAUNTER,   MISDREAVUS,1,"DORIS@@@@@@" ; Only place to find!
-	db VENONAT,   TANGELA,   2,"CRINKLES@@@" ; can find in wild
-	db GROWLITHE, HOUNDOUR,  2,"SPOT@@@@@@@" ; can find in wild
+	db NIDORINO,  NIDORINA,  0,"TERRY@@@@@@" ; Route 11 - Can find in wild
+	db ABRA,      SNEASEL,   0,"SHADOW@@@@@" ; Route 2 - Only place to find!
+	db PIKACHU,   TOGEPI,    1,"SHELLY@@@@@" ; Pewter Museum - Only place to find!
+	db PONYTA,    CHARMANDER,0,"FLARE@@@@@@" ; Cinnabar Island - Only place (if not starter)
+	db POLIWAG,   SQUIRTLE,  2,"CRUSHER@@@@" ; Vermillion City - Only place (if not starter)
+	db SLOWBRO,   LICKITUNG, 0,"MARC@@@@@@@" ; Route 18 - Can find in wild
+	db ODDISH,    BULBASAUR, 1,"FIONA@@@@@@" ; Cerulean City - Only place (if not starter)
+	db HAUNTER,   MISDREAVUS,1,"DORIS@@@@@@" ; Cinnabar Island - Only place to find!
+	db VENONAT,   TANGELA,   2,"CRINKLES@@@" ; Cinnabar Island - Can find in wild
+	db GROWLITHE, HOUNDOUR,  2,"SPOT@@@@@@@" ; Route 5 - Can find in wild
 
