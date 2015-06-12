f24da252c0e073cfa3d328e8bff01ed11c235085
TheFakeMateo <skeetendomateo@gmail.com>
2015-06-12 18:02:03 -0500

    Split Blaine Quizzes into separate file

--- text.asm
+++ text.asm
@@ -555,69 +555,7 @@ _GameOverText:: ; 88798 (22:4798)
 	line "GAME is over!"
 	done
 
-_CinnabarGymQuizIntroText:: ; 887b7 (22:47b7)
-	text "#MON Quiz!"
-
-	para "Get it right and"
-	line "the door opens to"
-	cont "the next room!"
-
-	para "Get it wrong and"
-	line "face a trainer!"
-
-	para "If you want to"
-	line "conserve your"
-	cont "#MON for the"
-	cont "GYM LEADER..."
-
-	para "Then get it right!"
-	line "Here we go!"
-	prompt
-
-_CinnabarQuizQuestionsText1:: ; 8886d (22:486d)
-	text "CATERPIE evolves"
-	line "into BUTTERFREE?"
-	done
-
-_CinnabarQuizQuestionsText2:: ; 88890 (22:4890)
-	text "There are 9"
-	line "certified #MON"
-	cont "LEAGUE BADGEs?"
-	done
-
-_CinnabarQuizQuestionsText3:: ; 888bb (22:48bb)
-	text "POLIWAG evolves 3"
-	line "times?"
-	done
-
-_CinnabarQuizQuestionsText4:: ; 888d5 (22:48d5)
-	text "Are thunder moves"
-	line "effective against"
-	cont "ground element-"
-	cont "type #MON?"
-	done
-
-_CinnabarQuizQuestionsText5:: ; 88915 (22:4915)
-	text "#MON of the"
-	line "same kind and"
-	cont "level are not"
-	cont "identical?"
-	done
-
-_CinnabarQuizQuestionsText6:: ; 88949 (22:4949)
-	text "TM28 contains"
-	line "TOMBSTONER?"
-	done
-
-_CinnabarGymQuizCorrectText:: ; 88964 (22:4964)
-	text "You're absolutely"
-	line "correct!"
-
-	para "Go on through!@@"
-
-_CinnabarGymQuizIncorrectText:: ; 8898f (22:498f)
-	text "Sorry! Bad call!"
-	prompt
+INCLUDE "text/quizzes.asm"
 
 _MagazinesText:: ; 889a1 (22:49a1)
 	text "#MON magazines!"
