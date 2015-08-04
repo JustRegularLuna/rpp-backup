bb4ec78276347df17f4751d1110d94b190abd917
TheFakeMateo <skeetendomateo@gmail.com>
2015-08-03 20:51:20 -0500

    Remove dummy items with badge names

--- text/maps/cerulean_badge_house.asm
+++ text/maps/cerulean_badge_house.asm
@@ -1,4 +1,4 @@
-_CeruleanHouse2Text_74e77:: ; 990c9 (26:50c9)
+_CeruleanHouse2Text1:: ; 990c9 (26:50c9)
 	text "#MON BADGEs"
 	line "are owned only by"
 	cont "skilled trainers."
@@ -8,91 +8,5 @@ _CeruleanHouse2Text_74e77:: ; 990c9 (26:50c9)
 
 	para "Those BADGEs have"
 	line "amazing secrets!"
-	prompt
-
-_CeruleanHouse2Text_74e7c:: ; 9913a (26:513a)
-	text "Now then..."
-
-	para "Which of the 8"
-	line "BADGEs should I"
-	cont "describe?"
 	done
-
-_CeruleanHouse2Text_74e81:: ; 99170 (26:5170)
-	text "Come visit me any"
-	line "time you wish."
-	done
-
-_CeruleanHouse2Text_74e96:: ; 99192 (26:5192)
-	text "The ATTACK of all"
-	line "#MON increases"
-	cont "a little bit."
-
-	para "It also lets you"
-	line "use FLASH any"
-	cont "time you desire."
-	prompt
-
-_CeruleanHouse2Text_74e9b:: ; 991f2 (26:51f2)
-	text "#MON up to L30"
-	line "will obey you."
-
-	para "Any higher, they"
-	line "become unruly!"
-
-	para "It also lets you"
-	line "use CUT outside"
-	cont "of battle."
-	prompt
-
-_CeruleanHouse2Text_74ea0:: ; 9925d (26:525d)
-	text "The SPEED of all"
-	line "#MON increases"
-	cont "a little bit."
-
-	para "It also lets you"
-	line "use FLY outside"
-	cont "of battle."
-	prompt
-
-_CeruleanHouse2Text_74ea5:: ; 992b8 (26:52b8)
-	text "#MON up to L50"
-	line "will obey you."
-
-	para "Any higher, they"
-	line "become unruly!"
-
-	para "It also lets you"
-	line "use STRENGTH out-"
-	cont "side of battle."
-	prompt
-
-_CeruleanHouse2Text_74eaa:: ; 9932a (26:532a)
-	text "The DEFENSE of all"
-	line "#MON increases"
-	cont "a little bit."
-
-	para "It also lets you"
-	line "use SURF outside"
-	cont "of battle."
-	prompt
-
-_CeruleanHouse2Text_74eaf:: ; 99388 (26:5388)
-	text "#MON up to L70"
-	line "will obey you."
-
-	para "Any higher, they"
-	line "become unruly!"
-	prompt
-
-_CeruleanHouse2Text_74eb4:: ; 993c7 (26:53c7)
-	text "Your #MON's"
-	line "SPECIAL abilities"
-	cont "increase a bit."
-	prompt
-
-_CeruleanHouse2Text_74eb9:: ; 993f5 (26:53f5)
-	text "All #MON will"
-	line "obey you!"
-	prompt
 
