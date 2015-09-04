e0ea60884ea0832e7986b1080fde36d895f07add
TheFakeMateo <skeetendomateo@gmail.com>
2015-09-04 15:43:56 -0500

    Edit Charmander trade to ask for an Onix

--- data/trades.asm
+++ data/trades.asm
@@ -4,7 +4,7 @@ TradeMons: ; 71b7b (1c:5b7b)
 	db NIDORINO,  NIDORINA,  0,"TERRY@@@@@@" ; Route 11 - Can find in wild
 	db ABRA,      SNEASEL,   0,"SHADOW@@@@@" ; Route 2 - Only place to find!
 	db PIKACHU,   TOGEPI,    1,"SHELLY@@@@@" ; Pewter Museum - Only place to find!
-	db SANDSHREW, CHARMANDER,0,"FLARE@@@@@@" ; Route 5 - Only place (if not starter)
+	db ONIX,      CHARMANDER,0,"FLARE@@@@@@" ; Route 5 - Only place (if not starter)
 	db POLIWAG,   SQUIRTLE,  2,"CRUSHER@@@@" ; Vermillion City - Only place (if not starter)
 	db SLOWBRO,   LICKITUNG, 0,"MARC@@@@@@@" ; Route 18 - Can find in wild
 	db ODDISH,    BULBASAUR, 1,"FIONA@@@@@@" ; Cerulean City - Only place (if not starter)
