32303f4ea4f77bec7b035964c0b9f874442fa1d8
TheFakeMateo <skeetendomateo@gmail.com>
2015-09-08 13:04:39 -0500

    Declare a constant for later use

--- constants/trainer_constants.asm
+++ constants/trainer_constants.asm
@@ -1,3 +1,5 @@
+TRAINER_START EQU $C8 ; This will eventually be changed to EQU NUM_POKEMON so I can have more than 200 more easily
+
 ; sometimes it's necessary to add $C8 to these values
 YOUNGSTER     EQU $01
 BUG_CATCHER   EQU $02
