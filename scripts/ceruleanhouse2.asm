d2bf926b90063f79bea721c2530ab4e02ea3fcc5
TheFakeMateo <skeetendomateo@gmail.com>
2015-09-07 20:55:45 -0500

    Add Berry Man in Cerulean

--- scripts/ceruleanhouse2.asm
+++ scripts/ceruleanhouse2.asm
@@ -1,11 +1,119 @@
 CeruleanHouse2Script: ; 74e09 (1d:4e09)
-	jp EnableAutoTextBoxDrawing
+	ld a, $1
+	ld [wAutoTextBoxDrawingControl], a
+	dec a
+	ld [wDoNotWaitForButtonPressAfterDisplayingText], a
+	ret
 
 CeruleanHouse2TextPointers: ; 74e13 (1d:4e13)
 	dw CeruleanHouse2Text1
 
 CeruleanHouse2Text1: ; 74e15 (1d:4e15)
-	TX_FAR _CeruleanHouse2Text1
+	db $8
+	ld hl, CeruleanHouse2Text_74e77
+	call PrintText
+	xor a
+	ld [wCurrentMenuItem], a
+	ld [wListScrollOffset], a
+.asm_74e23
+	ld hl, CeruleanHouse2Text_74e7c
+	call PrintText
+	ld hl, BerryItemList
+	call LoadItemList
+	ld hl, wStringBuffer2 + 11
+	ld a, l
+	ld [wList], a
+	ld a, h
+	ld [wList + 1], a
+	xor a
+	ld [wcf93], a
+	ld [wMenuItemToSwap], a
+	ld a, SPECIALLISTMENU
+	ld [wListMenuID], a
+	call DisplayListMenuID
+	jr c, .asm_74e60 ; 0x74e49 $15
+	ld hl, MenuTextPointers
+	ld a, [wcf91]
+	sub ORAN_BERRY
+	add a
+	ld d, $0
+	ld e, a
+	add hl, de
+	ld a, [hli]
+	ld h, [hl]
+	ld l, a
+	call PrintText
+	jr .asm_74e23 ; 0x74e5e $c3
+.asm_74e60
+	xor a
+	ld [wListScrollOffset], a
+	ld hl, CeruleanHouse2Text_74e81
+	call PrintText
+	jp TextScriptEnd
+
+BerryItemList: ; 74e6d (1d:4e6d)
+	db 10,ORAN_BERRY,SITRUS_BERRY,LEPPA_BERRY,PECHA_BERRY,RAWST_BERRY,ASPEAR_BERRY,CHESTO_BERRY,CHERI_BERRY,LUM_BERRY,SWEG_BERRY,$FF
+
+CeruleanHouse2Text_74e77: ; 74e77 (1d:4e77)
+	TX_FAR _CeruleanHouse2Text_74e77
+	db "@"
+
+CeruleanHouse2Text_74e7c: ; 74e7c (1d:4e7c)
+	TX_FAR _CeruleanHouse2Text_74e7c
+	db "@"
+
+CeruleanHouse2Text_74e81: ; 74e81 (1d:4e81)
+	TX_FAR _CeruleanHouse2Text_74e81
+	db "@"
+
+MenuTextPointers:
+	dw OranBerryText
+	dw SitrusBerryText
+	dw LeppaBerryText
+	dw PechaBerryText
+	dw RawstBerryText
+	dw AspearBerryText
+	dw ChestoBerryText
+	dw CheriBerryText
+    dw LumBerryText
+    dw SwegBerryText
+
+OranBerryText:
+	TX_FAR _OranBerryText
+	db "@"
+
+SitrusBerryText:
+	TX_FAR _SitrusBerryText
+	db "@"
+
+LeppaBerryText:
+	TX_FAR _LeppaBerryText
+	db "@"
+
+PechaBerryText:
+	TX_FAR _PechaBerryText
+	db "@"
+
+RawstBerryText:
+	TX_FAR _RawstBerryText
+	db "@"
+
+AspearBerryText:
+	TX_FAR _AspearBerryText
+	db "@"
+
+ChestoBerryText:
+	TX_FAR _ChestoBerryText
+	db "@"
+
+CheriBerryText:
+	TX_FAR _CheriBerryText
 	db "@"
     
+LumBerryText:
+    TX_FAR _LumBerryText
+    db "@"
     
+SwegBerryText:
+    TX_FAR _SwegBerryText
+    db "@"
--- text/maps/cerulean_badge_house.asm
+++ text/maps/cerulean_badge_house.asm
@@ -1,12 +1,79 @@
-_CeruleanHouse2Text1:: ; 990c9 (26:50c9)
-	text "#MON BADGEs"
-	line "are owned only by"
-	cont "skilled trainers."
+_CeruleanHouse2Text_74e77::
+	text "BERRY TREES grow"
+	line "all over KANTO."
 
-	para "I see you have"
-	line "at least one."
+	para "I'm sure you've"
+	line "seen them by now."
 
-	para "Those BADGEs have"
-	line "amazing secrets!"
+	para "Did you know they"
+	line "each do something"
+    cont "different?"
+	prompt
+
+_CeruleanHouse2Text_74e7c::
+	text "Now then..."
+
+	para "Which of the 10"
+	line "BERRIES should I"
+	cont "describe?"
 	done
 
+_CeruleanHouse2Text_74e81::
+	text "Come visit me any"
+	line "time you wish."
+	done
+
+_OranBerryText::
+	text "Restores 10 HP"
+	line "when used."
+	prompt
+
+_SitrusBerryText::
+	text "Restores 30 HP"
+	line "when used."
+	prompt
+
+_LeppaBerryText::
+	text "Restores 10 PP"
+	line "when used."
+	prompt
+
+_PechaBerryText::
+	text "Cures a #MON of"
+	line "being Poisoned."
+	prompt
+
+_RawstBerryText::
+	text "Cures a #MON of"
+	line "being Burned."
+	prompt
+
+_AspearBerryText::
+	text "Cures a #MON of"
+	line "being Frozen."
+	prompt
+
+_ChestoBerryText::
+	text "Wakes up a #MON"
+	line "that is Asleep."
+	prompt
+
+_CheriBerryText::
+	text "Cures a #MON of"
+	line "Paralysis."
+	prompt
+
+_LumBerryText::
+    text "Cures a #MON of"
+    line "any negative"
+    cont "status condition."
+    prompt
+    
+_SwegBerryText::
+    text "An extremely rare"
+    line "BERRY."
+    
+    para "It increases a"
+    line "#MON's level by"
+    cont "1 when used."
+    prompt
