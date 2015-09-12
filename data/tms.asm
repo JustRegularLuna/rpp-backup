48eb969c25fad6f380e48546d5a5360b1d58d4ad
TheFakeMateo <skeetendomateo@gmail.com>
2015-09-11 19:44:21 -0500

    Label where all TMs are as comments and mark some to be replaced

--- data/tms.asm
+++ data/tms.asm
@@ -1,56 +1,56 @@
 TechnicalMachines: ; 13773 (4:7773)
-	db DYNAMICPUNCH
-	db RAZOR_WIND ; replace?
-	db SWORDS_DANCE
-	db WHIRLWIND ; replace?
-	db MEGA_KICK
-	db TOXIC
-	db HORN_DRILL
-	db BODY_SLAM
-	db TAKE_DOWN
-	db DOUBLE_EDGE
-	db WATER_PULSE
-	db WATER_GUN ; replace?
-	db ICE_BEAM
-	db BLIZZARD
-	db HYPER_BEAM
-	db PAY_DAY
-	db SUBMISSION
-	db COUNTER
-	db SEISMIC_TOSS
-	db RAGE
-	db GIGA_DRAIN
-	db SOLARBEAM
-	db DRAGONBREATH
-	db THUNDERBOLT
-	db THUNDER
-	db EARTHQUAKE
-	db FISSURE
-	db DIG
-	db PSYCHIC_M
-	db TELEPORT ; replace?
-	db MIMIC
-	db DOUBLE_TEAM
-	db REFLECT
-	db HEADBUTT
-	db METRONOME ; replace?
-	db ROCK_TOMB
-	db FLAMETHROWER
-	db FIRE_BLAST
-	db SWIFT
-	db SKULL_BASH
-	db SHADOW_BALL
-	db DREAM_EATER
-	db DARK_PULSE
-	db REST
-	db THUNDER_WAVE
-	db PSYWAVE
-	db EXPLOSION
-	db ROCK_SLIDE
-	db TRI_ATTACK ; replace?
-	db SUBSTITUTE ; replace?
-	db CUT
-	db FLY
-	db SURF
-	db STRENGTH
-	db FLASH ; make a TM, use this slot for Rock Smash or Dive later
+	db DYNAMICPUNCH ; Victory Road
+	db RAZOR_WIND   ; Rocket Hideout, Celadon Dept //REPLACE
+	db SWORDS_DANCE ; Silph Co
+	db WHIRLWIND    ; Route 4 //REPLACE
+	db MEGA_KICK    ; Celadon Dept
+	db TOXIC        ; Koga Prize
+	db HORN_DRILL   ; Rocket Hideout
+	db BODY_SLAM    ; SS Anne
+	db TAKE_DOWN    ; Silph Co, Celadon Dept
+	db DOUBLE_EDGE  ; Rocket Hideout
+	db WATER_PULSE  ; Misty Prize
+	db WATER_GUN    ; Mt Moon //REPLACE
+	db ICE_BEAM     ; Celadon Dept Roof
+	db BLIZZARD     ; Pokemon Mansion
+	db HYPER_BEAM   ; Victory Road
+	db PAY_DAY      ; Route 12 //REPLACE
+	db SUBMISSION   ; Celadon Dept
+	db COUNTER      ; Celadon Dept
+	db SEISMIC_TOSS ; Route 25
+	db RAGE         ; Route 15 //REPLACE
+	db GIGA_DRAIN   ; Erika Prize
+	db SOLARBEAM    ; Pokemon Mansion
+	db DRAGONBREATH ; Victory Road
+	db THUNDERBOLT  ; Lt Surge Prize
+	db THUNDER      ; Power Plant
+	db EARTHQUAKE   ; Silph Co
+	db FISSURE      ; Giovanni Prize
+	db DIG          ; Cerulean City
+	db PSYCHIC_M    ; Saffron City
+	db TELEPORT     ; Route 9 //REPLACE (put Shadow Ball here)
+	db MIMIC        ; Saffron City
+	db DOUBLE_TEAM  ; Safari Zone, Celadon Dept
+	db REFLECT      ; Power Plant, Celadon Dept
+	db HEADBUTT     ; Mt Moon, Celadon Dept
+	db METRONOME    ; Pokemon Lab //REPLACE
+	db ROCK_TOMB    ; Brock Prize
+	db FLAMETHROWER ; Celadon Dept
+	db FIRE_BLAST   ; Blaine Prize
+	db SWIFT        ; Route 12
+	db SKULL_BASH   ; Safari Zone
+	db SHADOW_BALL  ; Celadon Dept //REPLACE (Put in canon TM slot)
+	db DREAM_EATER  ; Viridian City
+	db DARK_PULSE   ; Celadon City
+	db REST         ; SS Anne
+	db THUNDER_WAVE ; Route 24
+	db PSYWAVE      ; Sabrina Prize
+	db EXPLOSION    ; Victory Road
+	db ROCK_SLIDE   ; Celadon Dept Roof
+	db TRI_ATTACK   ; Celadon Dept Roof //REPLACE
+	db SUBSTITUTE   ; Safari Zone
+	db CUT          ; SS Anne
+	db FLY          ; Route 16
+	db SURF         ; Safari Zone
+	db STRENGTH     ; Fuchsia City
+	db FLASH        ; Route 2 //make Flash a TM, use this slot for Rock Smash or Dive later
