d9935f9f6bad3b0f99f8451bc065847d836ade09
TheFakeMateo <skeetendomateo@gmail.com>
2015-10-19 15:33:49 -0500

    Southern Island mapping and events

--- data/mapObjects/southernoutside.asm
+++ data/mapObjects/southernoutside.asm
@@ -2,18 +2,18 @@ SouthernIslandOutsideObject:
 	db $10 ; border block
 
 	db $3 ; warps
-	db $27, $e, $2, VERMILION_FERRY_DOCK
-	db $7, $16, $0, SOUTHERN_ISLAND_INSIDE
-	db $7, $17, $1, SOUTHERN_ISLAND_INSIDE
+	db $17, $e, $2, VERMILION_FERRY_DOCK
+	db $5, $e, $0, SOUTHERN_ISLAND_INSIDE
+	db $5, $f, $1, SOUTHERN_ISLAND_INSIDE
 
 	db $1 ; signs
-	db $20, $3, $3 ; SouthernIslandSignText
+	db $7, $10, $3 ; SouthernIslandSignText
 
 	db $2 ; people
-	db SPRITE_SS_CAPTAIN, $27 + 4, $d + 4, $ff, $d1, $1 ; SouthernIslandCaptainText
-	db SPRITE_BERRY_TREE, $e + 4, $14 + 4, $ff, $ff, $2 ; SouthernIslandBerryTreeText
+	db SPRITE_SS_CAPTAIN, $17 + 4, $d + 4, $ff, $d1, $1 ; SouthernIslandCaptainText
+	db SPRITE_BERRY_TREE, $7 + 4, $b + 4, $ff, $ff, $2 ; SouthernIslandBerryTreeText
 
 	; warp-to
-	EVENT_DISP SOUTHERN_ISLAND_OUTSIDE_WIDTH, $27, $e ; VERMILION_FERRY_DOCK
-	EVENT_DISP SOUTHERN_ISLAND_OUTSIDE_WIDTH, $7, $16 ; SOUTHERN_ISLAND_INSIDE
-	EVENT_DISP SOUTHERN_ISLAND_OUTSIDE_WIDTH, $7, $17 ; SOUTHERN_ISLAND_INSIDE
+	EVENT_DISP SOUTHERN_ISLAND_OUTSIDE_WIDTH, $17, $e ; VERMILION_FERRY_DOCK
+	EVENT_DISP SOUTHERN_ISLAND_OUTSIDE_WIDTH, $5, $e ; SOUTHERN_ISLAND_INSIDE
+	EVENT_DISP SOUTHERN_ISLAND_OUTSIDE_WIDTH, $5, $f ; SOUTHERN_ISLAND_INSIDE
