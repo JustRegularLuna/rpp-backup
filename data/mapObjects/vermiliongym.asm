21c2c116effb7d0d542aa74c3799d91b1349857a
TheFakeMateo <skeetendomateo@gmail.com>
2015-11-16 19:57:45 -0600

    Edit a trainer in Vermilion Gym

--- data/mapObjects/vermiliongym.asm
+++ data/mapObjects/vermiliongym.asm
@@ -10,7 +10,7 @@ VermilionGymObject: ; 0x5cbfe (size=58)
 	db $5 ; people
 	db SPRITE_SURGE, $1 + 4, $5 + 4, $ff, $d0, TRAINER | $1, LT__SURGE + TRAINER_START, $1
 	db SPRITE_GENTLEMAN, $6 + 4, $9 + 4, $ff, $d2, TRAINER | $2, GENTLEMAN + TRAINER_START, $3
-	db SPRITE_BLACK_HAIR_BOY_2, $8 + 4, $3 + 4, $ff, $d2, TRAINER | $3, ROCKER + TRAINER_START, $1
+	db SPRITE_BRUNETTE_GIRL, $8 + 4, $3 + 4, $ff, $d2, TRAINER | $3, ROCKER + TRAINER_START, $1
 	db SPRITE_SAILOR, $a + 4, $0 + 4, $ff, $d3, TRAINER | $4, SAILOR + TRAINER_START, $8
 	db SPRITE_GYM_HELPER, $e + 4, $4 + 4, $ff, $d0, $5 ; person
 
