77cbcc103f10b941e59e18959116b369b6a4473c
TheFakeMateo <skeetendomateo@gmail.com>
2015-11-20 22:49:12 -0600

    Pecca vs Petcha

--- text/maps/pewter_house_1.asm
+++ text/maps/pewter_house_1.asm
@@ -2,24 +2,26 @@ _PewterHouse1Text1:: ; 984ce (26:44ce)
 	text "NIDORAN: Bowbow!@@"
 
 _PewterHouse1Text2:: ; 984e1 (26:44e1)
-	text "NIDORAN sit!"
+	text "Here you go,"
+	line "NIDORAN!"
+	
+	para "Here's a nice"
+	line "PECHA BERRY for"
+	cont "you!"
 	done
 
 _PewterHouse1Text3:: ; 984ef (26:44ef)
-	text "Our #MON's an"
-	line "outsider, so it's"
-	cont "hard to handle."
+	text "My son doesn't"
+	line "agree with me on"
+	cont "how to pronounce"
+	cont "PECHA BERRY."
 
-	para "An outsider is a"
-	line "#MON that you"
-	cont "get in a trade."
-
-	para "It grows fast, but"
-	line "it may ignore an"
-	cont "unskilled trainer"
-	cont "in battle!"
-
-	para "If only we had"
-	line "some BADGEs..."
+	para "He thinks it's"
+	line "like PETCHA, but"
+	cont "I say it's PECCA."
+	
+	para "I think he must"
+	line "be listening to"
+	cont "GalacticElliot on"
+	cont "Twitter..."
 	done
-
