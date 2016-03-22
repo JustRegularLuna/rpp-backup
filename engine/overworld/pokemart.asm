be6566933caa5f01a3482325a1ffa53699f8db7c
Marcus Huderle <huderlem@gmail.com>
2016-03-21 21:53:28 -0700

    Show item descriptions in the Mart's BUY menu.

--- engine/overworld/pokemart.asm
+++ engine/overworld/pokemart.asm
@@ -133,7 +133,7 @@ DisplayPokemartDialogue_: ; 6c20 (1:6c20)
 	ld [wcf93],a
 	inc a ; a = 2 (PRICEDITEMLISTMENU)
 	ld [wListMenuID],a
-	call DisplayListMenuID
+	call DisplayListMenuID_Mart
 	jr c,.returnToMainPokemartMenu ; if the player closed the menu
 	ld a,$63
 	ld [wcf97],a
@@ -252,3 +252,344 @@ PokemartThankYouText: ; 6e39 (1:6e39)
 PokemartAnythingElseText: ; 6e3e (1:6e3e)
 	TX_FAR _PokemartAnythingElseText
 	db "@"
+
+DisplayListMenuID_Mart::
+	xor a
+	ld [H_AUTOBGTRANSFERENABLED],a ; disable auto-transfer
+	ld a,1
+	ld [hJoy7],a ; joypad state update flag
+	ld hl,wd730
+	set 6,[hl] ; turn off letter printing delay
+	xor a
+	ld [wMenuItemToSwap],a ; 0 means no item is currently being swapped
+	ld [wd12a],a
+	ld a,[wList]
+	ld l,a
+	ld a,[wList + 1]
+	ld h,a ; hl = address of the list
+	ld a,[hl] ; the first byte is the number of entries in the list
+	ld [wd12a],a
+	ld a,LIST_MENU_BOX
+	ld [wTextBoxID],a
+	callab DisplayTextBoxID ; draw the menu text box
+	call UpdateSprites ; disable sprites behind the text box
+; the code up to .skipMovingSprites appears to be useless
+	coord hl, 4, 2 ; coordinates of upper left corner of menu text box
+	lb de, 9, 14 ; height and width of menu text box
+	ld a,1 ; max menu item ID is 1 if the list has less than 2 entries
+	ld [wcc37],a
+	ld a,[wd12a]
+	cp a,2 ; does the list have less than 2 entries?
+	jr c,.setMenuVariables
+	ld a,2 ; max menu item ID is 2 if the list has at least 2 entries
+.setMenuVariables
+	ld [wMaxMenuItem],a
+	ld a,4
+	ld [wTopMenuItemY],a
+	ld a,5
+	ld [wTopMenuItemX],a
+	ld a,A_BUTTON | B_BUTTON | SELECT
+	ld [wMenuWatchedKeys],a
+	ld c,10
+	call DelayFrames
+	call PrintItemDescription_Mart
+
+DisplayListMenuIDLoop_Mart:: ; 2c53 (0:2c53)
+	xor a
+	ld [H_AUTOBGTRANSFERENABLED],a ; disable transfer
+	call PrintListMenuEntries
+	ld a,1
+	ld [H_AUTOBGTRANSFERENABLED],a ; enable transfer
+	call Delay3
+	ld a,[W_BATTLETYPE]
+	and a ; is it the Old Man battle?
+	jr z,.notOldManBattle
+.oldManBattle
+	ld a,"▶"
+	Coorda 5, 4 ; place menu cursor in front of first menu entry
+	ld c,80
+	call DelayFrames
+	xor a
+	ld [wCurrentMenuItem],a
+	coord hl, 5, 4
+	ld a,l
+	ld [wMenuCursorLocation],a
+	ld a,h
+	ld [wMenuCursorLocation + 1],a
+	jr .buttonAPressed
+.notOldManBattle
+	call LoadGBPal
+	call HandleMenuInput_Mart
+	push af
+	call PlaceMenuCursor
+	pop af
+	bit 0,a ; was the A button pressed?
+	jp z,.checkOtherKeys
+.buttonAPressed
+	ld a,[wCurrentMenuItem]
+	call PlaceUnfilledArrowMenuCursor
+
+; pointless because both values are overwritten before they are read
+	ld a,$01
+	ld [wd12e],a
+	ld [wd12d],a
+
+	xor a
+	ld [wcc37],a
+	ld a,[wCurrentMenuItem]
+	ld c,a
+	ld a,[wListScrollOffset]
+	add c
+	ld c,a
+	ld a,[wd12a]
+	and a ; is the list empty?
+	jp z,ExitListMenu_Mart ; if so, exit the menu
+	dec a
+	cp c ; did the player select Cancel?
+	jp c,ExitListMenu_Mart ; if so, exit the menu
+	ld a,c
+	ld [wWhichPokemon],a
+	ld a,[wListMenuID]
+	cp a,ITEMLISTMENU
+	jr nz,.skipMultiplying
+; if it's an item menu
+	sla c ; item entries are 2 bytes long, so multiply by 2
+.skipMultiplying
+	ld a,[wList]
+	ld l,a
+	ld a,[wList + 1]
+	ld h,a
+	inc hl ; hl = beginning of list entries
+	ld b,0
+	add hl,bc
+	ld a,[hl]
+	ld [wcf91],a
+	ld a,[wListMenuID]
+	and a ; is it a PC pokemon list?
+	jr z,.pokemonList
+	push hl
+	call GetItemPrice
+	pop hl
+	ld a,[wListMenuID]
+	cp a,ITEMLISTMENU
+	jr nz,.skipGettingQuantity
+; if it's an item menu
+	inc hl
+	ld a,[hl] ; a = item quantity
+	ld [wcf97],a
+.skipGettingQuantity
+	ld a,[wcf91]
+	ld [wd0b5],a
+	ld a,BANK(ItemNames)
+	ld [wPredefBank],a
+	call GetName
+	jr .storeChosenEntry
+.pokemonList
+	ld hl,wPartyCount
+	ld a,[wList]
+	cp l ; is it a list of party pokemon or box pokemon?
+	ld hl,wPartyMonNicks
+	jr z,.getPokemonName
+	ld hl, wBoxMonNicks ; box pokemon names
+.getPokemonName
+	ld a,[wWhichPokemon]
+	call GetPartyMonName
+.storeChosenEntry ; store the menu entry that the player chose and return
+	ld de,wcd6d
+	call CopyStringToCF4B ; copy name to wcf4b
+	ld a,1
+	ld [wd12e],a
+	ld a,[wCurrentMenuItem]
+	ld [wd12d],a
+	xor a
+	ld [hJoy7],a ; joypad state update flag
+	ld hl,wd730
+	res 6,[hl] ; turn on letter printing delay
+	ret
+.checkOtherKeys ; check B, SELECT, Up, and Down keys
+	bit 1,a ; was the B button pressed?
+	jp nz,ExitListMenu_Mart ; if so, exit the menu
+	bit 2,a ; was the select button pressed?
+	jp nz,HandleItemListSwapping ; if so, allow the player to swap menu entries
+	ld b,a
+	bit 7,b ; was Down pressed?
+	ld hl,wListScrollOffset
+	jr z,.upPressed
+.downPressed
+	ld a,[hl]
+	add a,3
+	ld b,a
+	ld a,[wd12a]
+	cp b ; will going down scroll past the Cancel button?
+	jp c,DisplayListMenuIDLoop_Mart
+	inc [hl] ; if not, go down
+	call PrintItemDescription_Mart
+	jp DisplayListMenuIDLoop_Mart
+.upPressed
+	ld a,[hl]
+	and a
+	jp z,DisplayListMenuIDLoop_Mart
+	dec [hl]
+	call PrintItemDescription_Mart
+	jp DisplayListMenuIDLoop_Mart
+
+PrintItemDescription_Mart::
+	push bc
+	push de
+	push hl
+	; Get item id.
+	ld a, [wCurrentMenuItem]
+	ld c, a
+	ld a, [wListScrollOffset]
+	add c
+	ld c, a
+	ld hl, wList
+	ld a, [hli]
+	ld e, a
+	ld a, [hl]
+	ld d, a
+	inc de  ; de = pointer to list
+	ld h, d
+	ld l, e
+	ld b, 0
+	add hl, bc
+	ld a, [hl]
+	ld d, a ; d = item id
+	callab FlashItemDescription_Mart
+	pop hl
+	pop de
+	pop bc
+	ret
+
+ExitListMenu_Mart:: ; 2e3b (0:2e3b)
+	ld a,[wCurrentMenuItem]
+	ld [wd12d],a
+	ld a,2
+	ld [wd12e],a
+	ld [wcc37],a
+	xor a
+	ld [hJoy7],a
+	ld hl,wd730
+	res 6,[hl]
+	xor a
+	ld [wMenuItemToSwap],a ; 0 means no item is currently being swapped
+	scf
+	ret
+
+HandleMenuInput_Mart:: ; 3abe (0:3abe)
+	xor a
+	ld [wd09b],a
+	ld a,[H_DOWNARROWBLINKCNT1]
+	push af
+	ld a,[H_DOWNARROWBLINKCNT2]
+	push af ; save existing values on stack
+	xor a
+	ld [H_DOWNARROWBLINKCNT1],a ; blinking down arrow timing value 1
+	ld a,$06
+	ld [H_DOWNARROWBLINKCNT2],a ; blinking down arrow timing value 2
+.loop1
+	xor a
+	ld [W_SUBANIMTRANSFORM],a ; counter for pokemon shaking animation
+	call PlaceMenuCursor
+	call Delay3
+.loop2
+	push hl
+	ld a,[wd09b]
+	and a ; is it a pokemon selection menu?
+	jr z,.getJoypadState
+	callba AnimatePartyMon ; shake mini sprite of selected pokemon
+.getJoypadState
+	pop hl
+	call JoypadLowSensitivity
+	ld a,[hJoy5]
+	and a ; was a key pressed?
+	jr nz,.keyPressed
+	push hl
+	hlCoord 18, 11 ; coordinates of blinking down arrow in some menus
+	call HandleDownArrowBlinkTiming ; blink down arrow (if any)
+	pop hl
+	ld a,[wMenuJoypadPollCount]
+	dec a
+	jr z,.giveUpWaiting
+	jr .loop2
+.giveUpWaiting
+; if a key wasn't pressed within the specified number of checks
+	pop af
+	ld [H_DOWNARROWBLINKCNT2],a
+	pop af
+	ld [H_DOWNARROWBLINKCNT1],a ; restore previous values
+	xor a
+	ld [wMenuWrappingEnabled],a ; disable menu wrapping
+	ret
+.keyPressed
+	xor a
+	ld [wcc4b],a
+	ld a,[hJoy5]
+	ld b,a
+	bit 6,a ; pressed Up key?
+	jr z,.checkIfDownPressed
+.upPressed
+	ld a,[wCurrentMenuItem] ; selected menu item
+	and a ; already at the top of the menu?
+	jr z,.alreadyAtTop
+.notAtTop
+	dec a
+	ld [wCurrentMenuItem],a ; move selected menu item up one space
+	call PrintItemDescription_Mart
+	jr .checkOtherKeys
+.alreadyAtTop
+	ld a,[wMenuWrappingEnabled]
+	and a ; is wrapping around enabled?
+	jr z,.noWrappingAround
+	ld a,[wMaxMenuItem]
+	ld [wCurrentMenuItem],a ; wrap to the bottom of the menu
+	jr .checkOtherKeys
+.checkIfDownPressed
+	bit 7,a
+	jr z,.checkOtherKeys
+.downPressed
+	ld a,[wCurrentMenuItem]
+	inc a
+	ld c,a
+	ld a,[wMaxMenuItem]
+	cp c
+	jr nc,.notAtBottom
+.alreadyAtBottom
+	ld a,[wMenuWrappingEnabled]
+	and a ; is wrapping around enabled?
+	jr z,.noWrappingAround
+	ld c,$00 ; wrap from bottom to top
+.notAtBottom
+	ld a,c
+	ld [wCurrentMenuItem],a
+	call PrintItemDescription_Mart
+.checkOtherKeys
+	ld a,[wMenuWatchedKeys]
+	and b ; does the menu care about any of the pressed keys?
+	jp z,.loop1
+.checkIfAButtonOrBButtonPressed
+	ld a,[hJoy5]
+	and A_BUTTON | B_BUTTON
+	jr z,.skipPlayingSound
+.AButtonOrBButtonPressed
+	push hl
+	ld hl,wFlags_0xcd60
+	bit 5,[hl]
+	pop hl
+	jr nz,.skipPlayingSound
+	ld a,(SFX_02_40 - SFX_Headers_02) / 3
+	call PlaySound ; play sound
+.skipPlayingSound
+	pop af
+	ld [H_DOWNARROWBLINKCNT2],a
+	pop af
+	ld [H_DOWNARROWBLINKCNT1],a ; restore previous values
+	xor a
+	ld [wMenuWrappingEnabled],a ; disable menu wrapping
+	ld a,[hJoy5]
+	ret
+.noWrappingAround
+	ld a,[wcc37]
+	and a ; should we return if the user tried to go past the top or bottom?
+	jr z,.checkOtherKeys
+	jr .checkIfAButtonOrBButtonPressed
--- text/item_descriptions.asm
+++ text/item_descriptions.asm
@@ -629,7 +629,7 @@ _CheriBerryDescription::
 	prompt
 
 _LumBerryDescription::
-	text "Cures a #MON of"
+	text "Cures #MON of"
 	line "any negative"
 	cont "status condition."
 	prompt
@@ -681,7 +681,8 @@ _HM05Description::
 	prompt
 
 _TM01Description::
-	text "TM01 - DYNAMICPUNCH"
+	text "TM01 -"
+	line "DYNAMICPUNCH"
 	prompt
 
 _TM02Description::
@@ -894,3 +895,761 @@ _TM49Description::
 _TM50Description::
 	text "TM50 - FLASH"
 	prompt
+
+_EmptyDescription::
+	text " "
+	done
+
+_MasterBallDescription_Mart::
+	text "The best BALL."
+	line "It never misses."
+	done
+
+_UltraBallDescription_Mart::
+	text "A BALL with high"
+	line "rate of success."
+	done
+
+_GreatBallDescription_Mart::
+	text "A BALL with"
+	line "decent success."
+	done
+
+_PokeBallDescription_Mart::
+	text "An item for"
+	line "catching #MON."
+	done
+
+_TownMapDescription_Mart::
+	text "A map of the"
+	line "region."
+	done
+
+_BicycleDescription_Mart::
+	text "Allows for quick"
+	line "traveling!"
+	done
+
+_SurfboardDescription_Mart::
+	text "..."
+	done
+
+_SafariBallDescription_Mart::
+	text "A BALL used in"
+	line "the SAFARI ZONE."
+	done
+
+_PokedexDescription_Mart::
+	text "A #MON"
+	line "encyclopedia."
+	done
+
+_MoonStoneDescription_Mart::
+	text "Evolves certain"
+	line "#MON."
+	done
+
+_AntidoteDescription_Mart::
+	text "Cures a poisoned"
+	line "#MON."
+	done
+
+_BurnHealDescription_Mart::
+	text "Cures a #MON"
+	line "of its burn."
+	done
+
+_IceHealDescription_Mart::
+	text "Thaws a frozen"
+	line "#MON."
+	done
+
+_AwakeningDescription_Mart::
+	text "Wakes a sleeping"
+	line "#MON."
+	done
+
+_ParalyzeHealDescription_Mart::
+	text "Cures a #MON"
+	line "of paralysis."
+	done
+
+_FullRestoreDescription_Mart::
+	text "Fully restores HP"
+	line "and status."
+	done
+
+_MaxPotionDescription_Mart::
+	text "Heals a #MON"
+	line "to its full HP."
+	done
+
+_HyperPotionDescription_Mart::
+	text "Heals a #MON"
+	line "by 200 HP."
+	done
+
+_SuperPotionDescription_Mart::
+	text "Heals a #MON"
+	line "by 50 HP."
+	done
+
+_PotionDescription_Mart::
+	text "Heals a #MON"
+	line "by 20 HP."
+	done
+
+_SafariBaitDescription_Mart::
+	text "Lures #MON in"
+	line "the SAFARI ZONE."
+	done
+
+_SafariRockDescription_Mart::
+	text "Angers #MON in"
+	line "the SAFARI ZONE."
+	done
+
+_OldSeaMapDescription_Mart::
+	text "A sea chart that"
+	line "marks an island."
+	done
+
+_MysticTicketDescription_Mart::
+	text "A ticket to"
+	line "NAVEL ROCK."
+	done
+
+_EonTicketDescription_Mart::
+	text "A ferry ticket to"
+	line "Southern Island."
+	done
+
+_TeruSama1ADescription_Mart::
+	text "..."
+	done
+
+_TeruSama1BDescription_Mart::
+	text "..."
+	done
+
+_TeruSama1CDescription_Mart::
+	text "..."
+	done
+
+_EscapeRopeDescription_Mart::
+	text "Use for escaping"
+	line "from caves, etc."
+	done
+
+_RepelDescription_Mart::
+	text "Repels #MON"
+	line "for 100 steps."
+	done
+
+_OldAmberDescription_Mart::
+	text "An ancient stone"
+	line "containing genes."
+	done
+
+_FireStoneDescription_Mart::
+	text "Evolves certain"
+	line "#MON."
+	done
+
+_ThunderStoneDescription_Mart::
+	text "Evolves certain"
+	line "#MON."
+	done
+
+_WaterStoneDescription_Mart::
+	text "Evolves certain"
+	line "#MON."
+	done
+
+_HPUpDescription_Mart::
+	text "Raises the HP of"
+	line "one #MON."
+	done
+
+_ProteinDescription_Mart::
+	text "Raises ATTACK of"
+	line "one #MON."
+	done
+
+_IronDescription_Mart::
+	text "Raises DEFENSE of"
+	line "one #MON."
+	done
+
+_CarbosDescription_Mart::
+	text "Raises SPEED of"
+	line "one #MON."
+	done
+
+_CalciumDescription_Mart::
+	text "Raises SPECIAL of"
+	line "one #MON."
+	done
+
+_RareCandyDescription_Mart::
+	text "Raises level of"
+	line "#MON by 1."
+	done
+
+_DomeFossilDescription_Mart::
+	text "A fossil of a"
+	line "seafloor #MON."
+	done
+
+_HelixFossilDescription_Mart::
+	text "A fossil of a"
+	line "seafloor #MON."
+	done
+
+_SecretKeyDescription_Mart::
+	text "Unlocks a door."
+	done
+
+_SunStoneDescription_Mart::
+	text "Evolves certain"
+	line "#MON."
+	done
+
+_BikeVoucherDescription_Mart::
+	text "Can be redeemed"
+	line "for a BICYCLE."
+	done
+
+_XAccuracyDescription_Mart::
+	text "Raises accuracy"
+	line "of attacks."
+	done
+
+_LeafStoneDescription_Mart::
+	text "Evolves certain"
+	line "#MON."
+	done
+
+_CardKeyDescription_Mart::
+	text "Opens something."
+	done
+
+_NuggetDescription_Mart::
+	text "A chunk of pure"
+	line "gold."
+	done
+
+_ThiefBallDescription_Mart::
+	text "A BALL capable of"
+	line "stealing #MON."
+	done
+
+_PokeDollDescription_Mart::
+	text "Use to escape"
+	line "from wild battle."
+	done
+
+_FullHealDescription_Mart::
+	text "Eliminates all"
+	line "status ailments."
+	done
+
+_ReviveDescription_Mart::
+	text "Revives a fainted"
+	line "#MON half HP."
+	done
+
+_MaxReviveDescription_Mart::
+	text "Revives a fainted"
+	line "#MON max HP."
+	done
+
+_GuardSpecDescription_Mart::
+	text "Prevents stat"
+	line "reduction."
+	done
+
+_SuperRepelDescription_Mart::
+	text "Repels #MON"
+	line "for 200 steps."
+	done
+
+_MaxRepelDescription_Mart::
+	text "Repels #MON"
+	line "for 250 steps."
+	done
+
+_DireHitDescription_Mart::
+	text "Raises critical"
+	line "hit chance."
+	done
+
+_CoinDescription_Mart::
+	text "Redeem for prizes"
+	line "at GAME CORNER."
+	done
+
+_FreshWaterDescription_Mart::
+	text "Heals a #MON"
+	line "by 50 HP."
+	done
+
+_SodaPopDescription_Mart::
+	text "Heals a #MON"
+	line "by 60 HP."
+	done
+
+_LemonadeDescription_Mart::
+	text "Heals a #MON"
+	line "by 80 HP."
+	done
+
+_SSTicketDescription_Mart::
+	text "A ticket to board"
+	line "the S.S. ANNE."
+	done
+
+_GoldTeethDescription_Mart::
+	text "A shiny pair of"
+	line "dentures."
+	done
+
+_XAttackDescription_Mart::
+	text "Raises ATTACK"
+	line "stat in battle."
+	done
+
+_XDefendDescription_Mart::
+	text "Raises DEFENSE"
+	line "stat in battle."
+	done
+
+_XSpeedDescription_Mart::
+	text "Raises SPEED"
+	line "stat in battle."
+	done
+
+_XSpecialDescription_Mart::
+	text "Raises SPECIAL"
+	line "stat in battle."
+	done
+
+_CoinCaseDescription_Mart::
+	text "A case that holds"
+	line "9,999 COINs."
+	done
+
+_OaksParcelDescription_Mart::
+	text "A package for"
+	line "PROF.OAK."
+	done
+
+_ItemFinderDescription_Mart::
+	text "Helps locate"
+	line "hidden items."
+	done
+
+_SilphScopeDescription_Mart::
+	text "An item that"
+	line "reveals ghosts."
+	done
+
+_PokeFluteDescription_Mart::
+	text "Wakes sleeping"
+	line "#MON."
+	done
+
+_LiftKeyDescription_Mart::
+	text "An elevator key."
+	done
+
+_EXPShareDescription_Mart::
+	text "Shares EXP among"
+	line "the team."
+	done
+
+_OldRodDescription_Mart::
+	text "An old, beat-up"
+	line "fishing rod."
+	done
+
+_GoodRodDescription_Mart::
+	text "A new, good-"
+	line "quality rod."
+	done
+
+_SuperRodDescription_Mart::
+	text "An awesome, high-"
+	line "tech fishing rod."
+	done
+
+_PPUpDescription_Mart::
+	text "Raises max PP of"
+	line "a selected move."
+	done
+
+_EtherDescription_Mart::
+	text "Restores 10 PP"
+	line "of one move."
+	done
+
+_MaxEtherDescription_Mart::
+	text "Fully restores PP"
+	line "a one move."
+	done
+
+_ElixerDescription_Mart::
+	text "Restores 10 PP to"
+	line "all moves."
+	done
+
+_MaxElixerDescription_Mart::
+	text "Fully restores PP"
+	line "of all moves."
+	done
+
+_FloorB2FDescription_Mart::
+	text "B2F"
+	done
+
+_FloorB1FDescription_Mart::
+	text "B1F"
+	done
+
+_Floor1FDescription_Mart::
+	text "1F"
+	done
+
+_Floor2FDescription_Mart::
+	text "2F"
+	done
+
+_Floor3FDescription_Mart::
+	text "3F"
+	done
+
+_Floor4FDescription_Mart::
+	text "4F"
+	done
+
+_Floor5FDescription_Mart::
+	text "5F"
+	done
+
+_Floor6FDescription_Mart::
+	text "6F"
+	done
+
+_Floor7FDescription_Mart::
+	text "7F"
+	done
+
+_Floor8FDescription_Mart::
+	text "8F"
+	done
+
+_Floor9FDescription_Mart::
+	text "9F"
+	done
+
+_Floor10FDescription_Mart::
+	text "10F"
+	done
+
+_Floor11FDescription_Mart::
+	text "11F"
+	done
+
+_FloorB4FDescription_Mart::
+	text "B4F"
+	done
+
+_OranBerryDescription_Mart::
+	text "Restores 10 HP"
+	line "when used."
+	done
+
+_SitrusBerryDescription_Mart::
+	text "Restores 30 HP"
+	line "when used."
+	done
+
+_LeppaBerryDescription_Mart::
+	text "Restores 10 PP"
+	line "when used."
+	done
+
+_PechaBerryDescription_Mart::
+	text "Cures a #MON of"
+	line "being Poisoned."
+	done
+
+_RawstBerryDescription_Mart::
+	text "Cures a #MON of"
+	line "being Burned."
+	done
+
+_AspearBerryDescription_Mart::
+	text "Cures a #MON of"
+	line "being Frozen."
+	done
+
+_ChestoBerryDescription_Mart::
+	text "Wakes up a #MON"
+	line "that is Asleep."
+	done
+
+_CheriBerryDescription_Mart::
+	text "Cures a #MON of"
+	line "Paralysis."
+	done
+
+_LumBerryDescription_Mart::
+	text "Cures #MON of"
+	line "status conditions"
+	done
+
+_SwegBerryDescription_Mart::
+	text "Raises level of"
+	line "#MON by 1."
+	done
+
+_GoHomeDescription_Mart::
+	text "Go home."
+	done
+
+_HM01Description_Mart::
+	text "HM01 - CUT"
+	done
+
+_HM02Description_Mart::
+	text "HM02 - FLY"
+	done
+
+_HM03Description_Mart::
+	text "HM03 - SURF"
+	done
+
+_HM04Description_Mart::
+	text "HM04 - STRENGTH"
+	done
+
+_HM05Description_Mart::
+	text "HM05 - DIVE"
+	done
+
+_TM01Description_Mart::
+	text "TM01 -"
+	line "DYNAMICPUNCH"
+	done
+
+_TM02Description_Mart::
+	text "TM02 - DRAGON CLAW"
+	done
+
+_TM03Description_Mart::
+	text "TM03 -"
+	line "SWORDS DANCE"
+	done
+
+_TM04Description_Mart::
+	text "TM04 - STEEL WING"
+	done
+
+_TM05Description_Mart::
+	text "TM05 - MEGA KICK"
+	done
+
+_TM06Description_Mart::
+	text "TM06 - TOXIC"
+	done
+
+_TM07Description_Mart::
+	text "TM07 - HORN DRILL"
+	done
+
+_TM08Description_Mart::
+	text "TM08 - BODY SLAM"
+	done
+
+_TM09Description_Mart::
+	text "TM09 - TAKE DOWN"
+	done
+
+_TM10Description_Mart::
+	text "TM10 - "
+	line "DOUBLE EDGE"
+	done
+
+_TM11Description_Mart::
+	text "TM11 -"
+	line "WATER PULSE"
+	done
+
+_TM12Description_Mart::
+	text "TM12 -"
+	line "MUDDY WATER"
+	done
+
+_TM13Description_Mart::
+	text "TM13 - ICE BEAM"
+	done
+
+_TM14Description_Mart::
+	text "TM14 - BLIZZARD"
+	done
+
+_TM15Description_Mart::
+	text "TM15 - HYPER BEAM"
+	done
+
+_TM16Description_Mart::
+	text "TM16 - IRON TAIL"
+	done
+
+_TM17Description_Mart::
+	text "TM17 - SUBMISSION"
+	done
+
+_TM18Description_Mart::
+	text "TM18 - COUNTER"
+	done
+
+_TM19Description_Mart::
+	text "TM19 -"
+	line "SEISMIC TOSS"
+	done
+
+_TM20Description_Mart::
+	text "TM20 - METAL CLAW"
+	done
+
+_TM21Description_Mart::
+	text "TM21 - GIGA DRAIN"
+	done
+
+_TM22Description_Mart::
+	text "TM22 - SOLARBEAM"
+	done
+
+_TM23Description_Mart::
+	text "TM23 -"
+	line "DRAGONBREATH"
+	done
+
+_TM24Description_Mart::
+	text "TM24 -"
+	line "THUNDERBOLT"
+	done
+
+_TM25Description_Mart::
+	text "TM25 - THUNDER"
+	done
+
+_TM26Description_Mart::
+	text "TM26 - EARTHQUAKE"
+	done
+
+_TM27Description_Mart::
+	text "TM27 - FISSURE"
+	done
+
+_TM28Description_Mart::
+	text "TM28 - DIG"
+	done
+
+_TM29Description_Mart::
+	text "TM29 - PSYCHIC"
+	done
+
+_TM30Description_Mart::
+	text "TM30 -"
+	line "SHADOW BALL"
+	done
+
+_TM31Description_Mart::
+	text "TM31 - MIMIC"
+	done
+
+_TM32Description_Mart::
+	text "TM32 -"
+	line "DOUBLE TEAM"
+	done
+
+_TM33Description_Mart::
+	text "TM33 - REFLECT"
+	done
+
+_TM34Description_Mart::
+	text "TM34 - HEADBUTT"
+	done
+
+_TM35Description_Mart::
+	text "TM35 -"
+	line "ANCIENTPOWER"
+	done
+
+_TM36Description_Mart::
+	text "TM36 - ROCK TOMB"
+	done
+
+_TM37Description_Mart::
+	text "TM37 -"
+	line "FLAMETHROWER"
+	done
+
+_TM38Description_Mart::
+	text "TM38 - FIRE BLAST"
+	done
+
+_TM39Description_Mart::
+	text "TM39 - SWIFT"
+	done
+
+_TM40Description_Mart::
+	text "TM40 - SKULL BASH"
+	done
+
+_TM41Description_Mart::
+	text "TM41 -"
+	line "AERIAL ACE"
+	done
+
+_TM42Description_Mart::
+	text "TM42 -"
+	line "DREAM EATER"
+	done
+
+_TM43Description_Mart::
+	text "TM43 - DARK PULSE"
+	done
+
+_TM44Description_Mart::
+	text "TM44 - REST"
+	done
+
+_TM45Description_Mart::
+	text "TM45 -"
+	line "THUNDER WAVE"
+	done
+
+_TM46Description_Mart::
+	text "TM46 - PSYWAVE"
+	done
+
+_TM47Description_Mart::
+	text "TM47 - EXPLOSION"
+	done
+
+_TM48Description_Mart::
+	text "TM48 - ROCK SLIDE"
+	done
+
+_TM49Description_Mart::
+	text "TM49 -"
+	line "DAZZLINGLEAM"
+	done
+
+_TM50Description_Mart::
+	text "TM50 - FLASH"
+	done
