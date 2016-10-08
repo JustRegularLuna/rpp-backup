14004ee50c574564887c56e3edf29401b3179a86
TheFakeMateo <skeetendomateo@gmail.com>
2016-10-08 17:23:18 -0500

    Improve the Pikachu text screen on New Game

--- engine/splashscreens/version_screen.asm
+++ engine/splashscreens/version_screen.asm
@@ -9,30 +9,68 @@ DisplayHackVersionScreen::
 	ld bc, VersionScreenTilesEnd - VersionScreenTiles
 	call CopyData	
 ; load the tilemap for the screen
-	ld hl, VersionScreenTilemap
-	ld de, wTileMap
-	ld bc, VersionScreenTilemapEnd - VersionScreenTilemap
-	call CopyData
+	call CleanPikachuScreen
 ; turn the screen back on
 	call EnableLCD
 	call Delay3
 	call GBPalNormal
+; display the version info text
+	hlCoord 2, 2
+	ld de, VersionScreenText
+	call PlaceString
 ; wait for the player to press A
+	call WaitForTextScrollButtonPress
+; load the tilemap for the screen again
+	call CleanPikachuScreen
+; display the second page
+	hlCoord 2, 2
+	ld de, VersionScreenText2
+	call PlaceString
+	; wait for the player to press A
+	call WaitForTextScrollButtonPress
+; load the tilemap for the screen again
+	call CleanPikachuScreen
+; display the third page
+	hlCoord 2, 6
+	ld de, VersionScreenText3
+	call PlaceString
+	; wait for the player to press A
 	call WaitForTextScrollButtonPress
 ; wipe the screen and go back
 	call ClearScreen
 	ret
+
+CleanPikachuScreen:
+	ld hl, VersionScreenTilemap
+	ld de, wTileMap
+	ld bc, VersionScreenTilemapEnd - VersionScreenTilemap
+	jp CopyData
 
 VersionScreenTiles:
 	INCBIN "gfx/version_screen_tiles.2bpp"
 VersionScreenTilesEnd:
 
 VersionScreenTilemap:
-IF DEF(_RED) ; Normal Rom
-	INCBIN "gfx/tilemaps/normal_screen.map"
-ENDC
-
-IF DEF(_BLUE) ; Hard Rom
-	INCBIN "gfx/tilemaps/hard_screen.map"
-ENDC
+	INCBIN "gfx/tilemaps/blank_text_screen.map"
 VersionScreenTilemapEnd:
+
+VersionScreenText:
+	db "Welcome to Red++", $4E
+	db "You are playing", $4E
+IF DEF(_BLUE) ; Hard Rom
+	db "The Hard Patch", $4E
+ELSE ; Normal Rom
+	db "The Normal Patch", $4E
+ENDC
+	db "Version 2.4 LTS@"
+
+VersionScreenText2:
+	db "Readme and FAQ", $4E
+	db "are found at:", $4E
+	db "www.github.com/", $4E
+	db "TheFakeMateo/", $4E
+	db "RedPlusPlus@"
+
+VersionScreenText3:
+	db "Have fun!", $4E
+	db "  - Mateo, 2016@"
